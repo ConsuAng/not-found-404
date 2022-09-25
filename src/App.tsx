0f3613fe29a49f665870dff084ee35cdcf6d3499
Consue <c.anguitasalinas@gmail.com>
2022-09-24 22:41:24 -0300

    💄 feat: 404 page view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import './App.css'
+import Container from './layout/Container'
+import scarecrow from './assets/scarecrow.png';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className='App pt-[20px] pb-[24px] pl-[24px] max-w-[1024px]'>
+      <div className='h-6 font-bold text-2xl font-sans tracking-[-0.08em] uppercase'>404 not found</div>
+      <Container>
+        <div className='flex flex-col items-center pt-[64px] lg:pt-0 lg:flex-row lg:items-center lg:justify-center'>
+          <img src={scarecrow}
+            className='w-[286px]'
+          />
+          <div className='w-full lg:pl-[119px]'>
+            <p className='text-5xl text-[#333333] pt-[60px] pb-[30px] w-[350px] font-bold font-mono tracking-[-0.035em]'>
+              I have bad news for you
+            </p> 
+
+            <p className='text-lg text-[#4F4F4F] w-[283px] font-normal tracking-[-0.035em]'>
+              The page you are looking for might be removed or is temporarily unavailable
+            </p>
+          <div className='pt-[66px] w-full'>
+            <button className='bg-[#333333] text-[#FFFFFF] px-[43px] py-[24px] racking-[-0.035em] font-bold text-sm uppercase'>
+              Back to homepage
+            </button>
+          </div>
+          </div>
+        </div>
+      </Container>
+      <div className='h-6 font-bold text-[#BDBDBD] text-xs font-serif lg:text-center'>created by Consuelo Anguita - devChallenges.io</div>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inconsolata', ...defaultTheme.fontFamily.sans],
+        mono: ['Space Mono', ...defaultTheme.fontFamily.mono],
+        serif: ['Montserrat', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
